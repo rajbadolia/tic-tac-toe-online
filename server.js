102a343e61ed5a1e78f3c941f78d5e348867fc52
Raj Badolia <32016972+rajbadolia@users.noreply.github.com>
2026-02-11 02:39:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET","POST"]
+  },
+  transports: ["websocket","polling"]
+});
+
 
 app.use(express.static('public'));
 
